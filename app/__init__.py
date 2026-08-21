9a0996492b279fcca023c413778749e75be7efc1
Tony Gardel Campos Braga <tony.braga@el.com.br>
2026-08-21 10:41:17 -0300

    Corrigir inicialização dos blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -5,19 +5,30 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 login_manager = LoginManager()
 
-def create_app():
 
+@login_manager.user_loader
+def load_user(user_id):
+    from app.usuarios.models import Usuario
+    return Usuario.query.get(int(user_id))
+
+
+def create_app():
     app = Flask(__name__)
 
-    app.config.from_object("config.Config")
+    app.config["SECRET_KEY"] = "sorroche-secret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sorroche.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
-
     login_manager.init_app(app)
 
-    from .dashboard.routes import dashboard
+    login_manager.login_view = "usuarios.login"
 
+    from app.dashboard.routes import dashboard
     app.register_blueprint(dashboard)
+
+    from app.usuarios.routes import usuarios
+    app.register_blueprint(usuarios)
 
     with app.app_context():
         db.create_all()
